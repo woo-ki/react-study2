5b0895a54c03e3c9ce8477781858aa2483f15021
김영욱 <ziziza93@naver.com>
2021-11-13 00:14:01 +0900

    부트스트랩 사용법

--- src/App.js
+++ src/App.js
@@ -1,9 +1,54 @@
 import './App.css';
+import {Button, Container, Nav, Navbar, NavDropdown} from "react-bootstrap";
 
 function App() {
 	return (
 		<div className="App">
-
+			<Navbar bg="light" expand="lg">
+				<Container>
+					<Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+					<Navbar.Toggle aria-controls="basic-navbar-nav" />
+					<Navbar.Collapse id="basic-navbar-nav">
+						<Nav className="me-auto">
+							<Nav.Link href="#home">Home</Nav.Link>
+							<Nav.Link href="#link">Link</Nav.Link>
+							<NavDropdown title="Dropdown" id="basic-nav-dropdown">
+								<NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+								<NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+								<NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+								<NavDropdown.Divider />
+								<NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+							</NavDropdown>
+						</Nav>
+					</Navbar.Collapse>
+				</Container>
+			</Navbar>
+			<div className="jumbotron background">
+				<h1>20% Season Off</h1>
+				<p>이거시야 이거시야~</p>
+				<p>
+					<Button variant={"primary"}>Learn more</Button>
+				</p>
+			</div>
+			<div className="container">
+				<div className="row">
+					<div className="col-md-4">
+						<img src="https://codingapple1.github.io/shop/shoes1.jpg" width={"100%"}/>
+						<h4>상품명</h4>
+						<p>상품설명 &amp; 가격</p>
+					</div>
+					<div className="col-md-4">
+						<img src="https://codingapple1.github.io/shop/shoes2.jpg" width={"100%"}/>
+						<h4>상품명</h4>
+						<p>상품설명 &amp; 가격</p>
+					</div>
+					<div className="col-md-4">
+						<img src="https://codingapple1.github.io/shop/shoes3.jpg" width={"100%"}/>
+						<h4>상품명</h4>
+						<p>상품설명 &amp; 가격</p>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
