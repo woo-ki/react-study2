131ada2e813a09f59c9419f8b2304f1f99012eec
김영욱 <ziziza93@naver.com>
2021-11-14 00:02:09 +0900

    router 기초 사용법
    ㄴ BrowserRouter와 HashRouter 두가지 종류
    ㄴ 해시는 정적 사이트에 적합, 브라우저는 동적사이트에 적합
    ㄴ 패스와 정확히 일치해야만 표기되는 속성은 exact

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import {useState} from "react";
 import {Button, Container, Nav, Navbar, NavDropdown} from "react-bootstrap";
 import data from './assets/data/data';
+import {Link, Route, Switch} from 'react-router-dom';
 
 function App() {
 	const [shoes, setShoes] = useState(data);
@@ -27,24 +28,44 @@ function App() {
 					</Navbar.Collapse>
 				</Container>
 			</Navbar>
-			<div className="jumbotron background">
-				<h1>20% Season Off</h1>
-				<p>이거시야 이거시야~</p>
-				<p>
-					<Button variant={"primary"}>Learn more</Button>
-				</p>
-			</div>
-			<div className="container">
-				<div className="row">
-					{
-						shoes.map((x, i) => {
-							return (
-								<Product product={x} key={i} />
-							)
-						})
-					}
+
+			<Route exact={true} path={"/"}>
+				<div className="jumbotron background">
+					<h1>20% Season Off</h1>
+					<p>이거시야 이거시야~</p>
+					<p>
+						<Button variant={"primary"}>Learn more</Button>
+					</p>
 				</div>
-			</div>
+				<div className="container">
+					<div className="row">
+						{
+							shoes.map((x, i) => {
+								return (
+									<Product product={x} key={i} />
+								)
+							})
+						}
+					</div>
+				</div>
+			</Route>
+			<Route path={"/detail"}>
+				<div className="container">
+					<div className="row">
+						<div className="col-md-6">
+							<img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+						</div>
+						<div className="col-md-6 mt-4">
+							<h4 className="pt-5">상품명</h4>
+							<p>상품설명</p>
+							<p>120000원</p>
+							<button className="btn btn-danger">주문하기</button>
+						</div>
+					</div>
+				</div>
+			</Route>
+			{/*<Route path={"/test"} component={Modal}></Route>*/}
+
 		</div>
 	);
 }
