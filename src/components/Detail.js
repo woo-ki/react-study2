f9e550ffd8785c0828f9a8b27bbd4a278b1ff63d
김영욱 <ziziza93@naver.com>
2021-11-14 02:47:30 +0900

    컴포넌트의 생명주기중(등장,렌더링,퇴장등등) 동작을 요청하는 LifeCycleHook, useEffect
    ㄴ 라이프사이클훅은 class형 (과거) useEffect는 function형 (최근)
    ㄴ useEffect만 쓸 경우 마운트, 업데이트 모든경우에 다 실행됨
    ㄴ 중괄호[]를 통해서 특정 스테이트가 업데이트 될때만 실행해줄 수 있음
    ㄴ []만 넣어두면 컴포넌트 업데이트는 반응하지 않고 생성,제거시에만 반응함

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,6 +1,7 @@
 import {useHistory, useParams} from 'react-router-dom';
 import styled from 'styled-components';
 import '../assets/css/detail.scss';
+import {useEffect, useState} from 'react';
 
 const Box = styled.div`
 	padding : 20px;
@@ -13,20 +14,42 @@ const Title = styled.h4`
 
 const Detail = (props) => {
 
+	const [alertIsOpened, setAlertIsOpened] = useState(true);
+
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			setAlertIsOpened(false);
+			clearTimeout(timer);
+		}, 2000);
+
+		return () => {
+			clearTimeout(timer);
+			// console.log('난 이제 없어질게!(unmount)');
+		};
+	}, []);
+
+	useEffect(() => {
+		// 여러개 작성할 수 있음! 다만 위부터 순서대로 실행됨
+	});
+
 	const history = useHistory();
 	const {id} = useParams();
 	const product = props.shoes.find((x) => Number(x.id) === Number(id));
-
-
 
 	return (
 		<div className="container">
 			<Box>
 				<Title className={"red"}>상세페이지</Title>
 			</Box>
-			<div className={"my_alert"}>
-				<p>재고가 얼마 남지 않았습니다!</p>
-			</div>
+			{
+				alertIsOpened
+					? (
+						<div className={"my_alert2"} id={"my-alert"}>
+							<p>재고가 얼마 남지 않았습니다!</p>
+						</div>
+					)
+					: null
+			}
 			<div className="row">
 				<div className="col-md-6">
 					<img src={`https://codingapple1.github.io/shop/shoes${product.id + 1}.jpg`} alt={"/"} width="100%"/>
